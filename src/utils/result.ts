8ea2ca4d9464ce4974d00a97ecd12c3a7c19b31b
Noodle Cats <terrariaplay573@gmail.com>
2026-04-12 22:34:30 +0800

    Add Ok() overloads and make Result.value non-nullable

--- src/utils/result.ts
+++ src/utils/result.ts
@@ -1,10 +1,13 @@
 export type Result<T, E = Error> =
-  | { ok: true; value?: T }
+  | { ok: true; value: T }
   | { ok: false; error: E };
 
-export function Ok<T>(value?: T): Result<T, never> {
-  return { ok: true, value };
+export function Ok(): Result<void, never>;
+export function Ok<T>(value: T): Result<T, never>;
+export function Ok<T>(value?: T): Result<T | void, never> {
+  return { ok: true, value: value as T | void };
 }
+
 export function Err<E>(error: E): Result<never, E> {
   return { ok: false, error };
 }
