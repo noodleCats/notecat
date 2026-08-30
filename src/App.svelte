6bbef0141f37382e96d7c20b5648dd56514ba6e5
Noodle Cats <terrariaplay573@gmail.com>
2026-08-30 15:51:42 +0800

    Reorganize app imports

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { notekeeper } from "./app/notekeeper.svelte";
-  import Header from "./components/Header.svelte";
-  import Sidebar from "./components/Sidebar.svelte";
-  import Editor from "./components/Editor.svelte";
-  import StatusBar from "./components/StatusBar.svelte";
-  import EmptyState from "./components/EmptyState.svelte";
-  import Modal from "./components/Modal.svelte";
-  import { sidebarState } from "./app/state/sidebar.svelte";
-  import { modalState } from "./app/state/modal.svelte";
   import { registerAppShortcuts } from "./app/shortcuts";
   import { setDateUpdateInterval } from "./app/state/time.svelte";
   import { addKeyboardEventListener } from "./app/state/keyboard.svelte";
+  import { sidebarState } from "./app/state/sidebar.svelte";
+  import { modalState } from "./app/state/modal.svelte";
+  import Header from "./components/Header.svelte";
+  import Sidebar from "./components/Sidebar.svelte";
+  import Editor from "./components/Editor.svelte";
+  import EmptyState from "./components/EmptyState.svelte";
+  import Modal from "./components/Modal.svelte";
+  import StatusBar from "./components/StatusBar.svelte";
 
   onMount(() => {
     const unregisterShortcuts = registerAppShortcuts();
